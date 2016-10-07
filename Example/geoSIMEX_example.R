b1e0ef6fee0a719be0b87efb0011f9945c03e2cd
ramarty <ramarty@email.wm.edu>
2016-10-07 10:53:32 -0400

    Update Data

--- Example/geoSIMEX_example.R
+++ Example/geoSIMEX_example.R
@@ -27,50 +27,60 @@ coordinates(uga.aiddata) <- ~longitude+latitude
 proj4string(uga.aiddata) = CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
 
 ##### Putting ADM heirarchy into AidData data
-  # NOTE: Can typically use getData to get ADM info; however, issue with
-  # server where this comes from... so might be resolved later.
-
-setwd("~/Desktop/AidData/MeasureErrorsInEx/geoSIMEX/geoSIMEX/Example/UGA_adm_shp")
-uga.3 <- readOGR(dsn = ".", layer = "UGA_adm3")
-
-aiddata.gadm.over <- over(uga.aiddata, uga.3)
-
-uga.aiddata$NAME_0 <- aiddata.gadm.over$NAME_0
-uga.aiddata$NAME_1 <- aiddata.gadm.over$NAME_1
-uga.aiddata$NAME_2 <- aiddata.gadm.over$NAME_2
-uga.aiddata$NAME_3 <- aiddata.gadm.over$NAME_3
-uga.aiddata <- uga.aiddata@data
-
-write.csv(uga.aiddata, "~/Desktop/AidData/MeasureErrorsInEx/geoSIMEX/geoSIMEX/Example/uga_aiddata_gadm.csv")
-
-
-
-
-
-shape <- readOGR(dsn = ".", layer = "~/workspace/SHAPEFILE")
-
-# Get Uganda GADM Data to Match with AidData Data
-uga.0 <- getData('GADM', country='UGA', level=0)
-uga.1 <- getData('GADM', country='UGA', level=1)
-uga.2 <- getData('GADM', country='UGA', level=2)
+  # NOTE: Can typically use getData to get ADM info; however, issue with server where this comes 
+  # from... so might be resolved later? Consequently, I'm importing from my local drive. And
+  # exporting the resulting dataset to github.
+if(FALSE){
+  setwd("~/Desktop/AidData/MeasureErrorsInEx/geoSIMEX/geoSIMEX/Example/UGA_adm_shp")
+  uga.3 <- readOGR(dsn = ".", layer = "UGA_adm3")
+  
+  aiddata.gadm.over <- over(uga.aiddata, uga.3)
+  
+  uga.aiddata$NAME_0 <- aiddata.gadm.over$NAME_0
+  uga.aiddata$NAME_1 <- aiddata.gadm.over$NAME_1
+  uga.aiddata$NAME_2 <- aiddata.gadm.over$NAME_2
+  uga.aiddata$NAME_3 <- aiddata.gadm.over$NAME_3
+  uga.aiddata <- uga.aiddata@data
+  
+  write.csv(uga.aiddata, "~/Desktop/AidData/MeasureErrorsInEx/geoSIMEX/geoSIMEX/Example/uga_aiddata_gadm.csv")
+} else{
+  uga.aiddata <- read.csv("https://raw.githubusercontent.com/ramarty/geoSIMEX/master/Example/uga_aiddata_gadm.csv")
+}
 
 
+uga.3$area <- area(uga.3)
 
+uga.adm3.df_a <- merge(uga.adm3.df, uga.3, by=c("NAME_0", "NAME_1", "NAME_2", "NAME_3"))
 
+write.csv(uga.adm3.df, "~/Desktop/AidData/MeasureErrorsInEx/geoSIMEX/geoSIMEX/Example/merge_uga_adm3.csv")
 
+# --- --- --- --- --- --- --- --- --- --- #
+##### Checks on Data #####
+# --- --- --- --- --- --- --- --- --- --- #
 
-
-
-
-
-# 1. get ADM data on AidData data to match uga.adm3.df data
-# 2. do a check to make sure sure all names in aiddata data are in uga.adm3.df data
-
-
+# Making sure all ADM names in aiddata dataset are in ROI-level dataset
+table(uga.aiddata$NAME_1 %in% uga.adm3.df$NAME_1)
+uga.aiddata[uga.aiddata$NAME_1 %in% uga.adm3.df$NAME_1 == FALSE,]
+uga.aiddata <- uga.aiddata[!is.na(uga.aiddata$NAME_1),]
+table(uga.aiddata$NAME_1 %in% uga.adm3.df$NAME_1)
 
 # --- --- --- --- --- --- --- --- --- --- #
 ##### Analysis #####
 # --- --- --- --- --- --- --- --- --- --- #
+
+##### Calculating Expected Aid in ROI
+subcountyData$trueAid <- expected_aid_ROI(aidData=uga.aiddata, 
+                                          roiData=uga.adm3.df, 
+                                          probAidAssume=probAid_equal, 
+                                          dollar_set=aidData$aid, 
+                                          aid.precision.code="PC", 
+                                          roi.pc1.name="subcounty", 
+                                          roi.pc2.name="county", 
+                                          roi.pc3.name="district", 
+                                          roi.pc4.name="region", 
+                                          roi.pc5.name="region", 
+                                          roi.pc6.name="country", 
+                                          aid.pc1.centroid.name="trueSubcounty")
 
 geoSIMEX_model <- geoSIMEX(model = naive_model, 
                            geoSIMEXvariable = "expectedAid", 
