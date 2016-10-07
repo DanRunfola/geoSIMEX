e91143c07976228c8229533124aec9d052926d2e
ramarty <ramarty@email.wm.edu>
2016-10-06 23:42:50 -0400

    Adding Data

---
+++ Example/geoSIMEX_example.R
@@ -0,0 +1,11 @@
+# Example Using geoSIMEX
+# AidData, REU
+# Last Updated: 10/7/2016
+
+# --- --- --- --- --- --- --- --- --- --- #
+##### Loading Packages and Data #####
+# --- --- --- --- --- --- --- --- --- --- #
+
+library(RCurl)
+eval(expr=parse(text=getURL("https://raw.githubusercontent.com/ramarty/geoSIMEX/master/R/geoSIMEX.R")))
+
