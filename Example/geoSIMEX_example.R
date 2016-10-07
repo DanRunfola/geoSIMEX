b7c87158a213d78682b8c7ea4f50d216b9ed1cef
ramarty <ramarty@email.wm.edu>
2016-10-07 10:32:40 -0400

    Add Shapefiles

--- Example/geoSIMEX_example.R
+++ Example/geoSIMEX_example.R
@@ -8,6 +8,7 @@
 # --- --- --- --- --- --- --- --- --- --- #
 library(raster)
 library(sp)
+library(rgdal)
 
 source("https://raw.githubusercontent.com/ramarty/geoSIMEX/master/R/geoSIMEX.R")
 uga.adm3.df <- read.csv("https://raw.githubusercontent.com/ramarty/geoSIMEX/master/Example/merge_uga_adm3.csv")
@@ -17,19 +18,42 @@ uga.aiddata <- read.csv("https://raw.githubusercontent.com/ramarty/geoSIMEX/mast
 ##### Prepping Data for Analysis #####
 # --- --- --- --- --- --- --- --- --- --- #
 
+##### Precision Code 6,8 projects don't have lat/long; give random lat/long
 uga.aiddata$longitude[is.na(uga.aiddata$longitude)] <- uga.aiddata$longitude[!is.na(uga.aiddata$longitude)][1]
 uga.aiddata$latitude[is.na(uga.aiddata$latitude)] <- uga.aiddata$latitude[!is.na(uga.aiddata$latitude)][1]
 
+##### Projecting AidData Data
 coordinates(uga.aiddata) <- ~longitude+latitude
-proj4string(uga.aiddata) = CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")
+proj4string(uga.aiddata) = CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
 
-over(uga.aiddata, uga.1)
+##### Putting ADM heirarchy into AidData data
+  # NOTE: Can typically use getData to get ADM info; however, issue with
+  # server where this comes from... so might be resolved later.
+
+setwd("~/Desktop/AidData/MeasureErrorsInEx/geoSIMEX/geoSIMEX/Example/UGA_adm_shp")
+uga.3 <- readOGR(dsn = ".", layer = "UGA_adm3")
+
+aiddata.gadm.over <- over(uga.aiddata, uga.3)
+
+uga.aiddata$NAME_0 <- aiddata.gadm.over$NAME_0
+uga.aiddata$NAME_1 <- aiddata.gadm.over$NAME_1
+uga.aiddata$NAME_2 <- aiddata.gadm.over$NAME_2
+uga.aiddata$NAME_3 <- aiddata.gadm.over$NAME_3
+uga.aiddata <- uga.aiddata@data
+
+write.csv(uga.aiddata, "~/Desktop/AidData/MeasureErrorsInEx/geoSIMEX/geoSIMEX/Example/uga_aiddata_gadm.csv")
+
+
+
+
+
+shape <- readOGR(dsn = ".", layer = "~/workspace/SHAPEFILE")
 
 # Get Uganda GADM Data to Match with AidData Data
 uga.0 <- getData('GADM', country='UGA', level=0)
 uga.1 <- getData('GADM', country='UGA', level=1)
 uga.2 <- getData('GADM', country='UGA', level=2)
-uga.3 <- getData('GADM', country='UGA', level=3)
+
 
 
 
